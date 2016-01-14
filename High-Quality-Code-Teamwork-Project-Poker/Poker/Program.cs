e7b4c607ad6006d736ce04ea0b5392876d9eac1b
Xzahn <tony.d.borisov@gmail.com>
2016-01-14 02:46:38 +0200

    Revert "Changing Form1.cs to PokerTable.cs"
    This reverts commit 688333ea9d8c5f7bcb42963827403413b3066a1f.

--- High-Quality-Code-Teamwork-Project-Poker/Poker/Program.cs
+++ High-Quality-Code-Teamwork-Project-Poker/Poker/Program.cs
@@ -16,7 +16,7 @@ namespace Poker
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new PokerTable());
+            Application.Run(new Form1());
         }
     }
 }
