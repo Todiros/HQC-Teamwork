066ee198e7d5072cb5c4a8732f81168a6e95fbfd
Sevgin Mustafov <sevgin95@gmail.com>
2016-01-20 16:38:46 +0200

    DataBase interface made

---
+++ High-Quality-Code-Teamwork-Project-Poker/Poker/IDataBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Poker
+{
+    public interface IDataBase
+    {
+        void AddBools(bool boolean);
+
+        void BoolsRemoveAt(int index);
+
+        void BoolsInsert(int index, bool? obj);
+
+        bool Contains(string checkedWinner);
+
+        void AddCheckedWinners(string checkedWinner);
+
+        void AddInts(int integer);
+
+        void ClearInts();
+
+        void ClearBools();
+
+        void ClearCheckedWinners();
+
+        void ToArray();
+
+        int IntsLenght();
+
+        int BoolsCount(Func<int, bool> predicate);
+
+        int IndexOf(bool? obj);
+    }
+}
